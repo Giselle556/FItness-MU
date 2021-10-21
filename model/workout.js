a13a6093175d1c298011b7cdf2c3e79f9941a57e
Giselle556 <giselleborbolla@yahoo.com>
2021-10-20 23:50:40 -0700

    adding workoutjs

--- model/workout.js
+++ model/workout.js
@@ -5,6 +5,35 @@ const workoutSchema = new Schema({
     date: {
         type: Date,
         default: Date.now
-    }
-    
-})
+    },
+    exercises:[
+        {
+           type:{
+              type:String,
+              required: " Enter a Workout Type!"
+           },
+           name:{
+              type: String,
+              required: "Enter a Exercise Name!"
+           },
+           duration:{
+              type:Number,
+           },
+           weight:{
+              type: Number
+           },
+           reps:{
+              type: Number
+           },
+           sets: {
+              type: Number
+           }, 
+           distance:{
+              type: Number
+           }
+        }
+     ]
+  });
+ const Workout = mongoose.model("workout", workoutSchema);
+ 
+ module.exports = Workout;
