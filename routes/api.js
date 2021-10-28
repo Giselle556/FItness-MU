86334fc45f6bfe1e83fe1d758125127ed778ea1a
Giselle556 <giselleborbolla@yahoo.com>
2021-10-28 13:35:22 -0700

    added to the routes but need to fix

--- routes/api.js
+++ routes/api.js
@@ -2,5 +2,64 @@ const router = require("express").Router();
 const Workout = require("../model/workout");
 
 router.get("/api/workouts/range", (req,res) => {
-    
-})
+    Workout.Workout.find({})
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+router.post("/api/workouts", (req, res) => {
+  Workout.Workout.create({})
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+router.put("/api/workouts/:id", (req, res) => {
+  const id = req.params.id;
+  const body = req.body;
+
+  Workout.Workout.findOneAndUpdate({ _id: id }, { $push: { exercises: body } })
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+router.put("/api/workouts/:id", (req, res) => {
+  const id = req.params.id;
+  const body = req.body;
+
+  Workout.Workout.findOneAndUpdate(
+    { _id: id },
+    { $push: { exercises: body } },
+    { new: true, runValidators: true }
+  )
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+router.get("/api/workouts/range", (req, res) => {
+  Workout.Workout.find({})
+    .limit(7)
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+module.exports = router;
