fa8d3c46d64cae5baede91e6ee29a39810877e64
Giselle556 <giselleborbolla@yahoo.com>
2021-11-03 20:54:34 -0700

    changed workout.js

--- model/workout.js
+++ model/workout.js
@@ -1,59 +1,51 @@
 const mongoose = require("mongoose");
+
 const Schema = mongoose.Schema;
 
-const workoutSchema = new Schema(
-    {
-      day: {
-        type: Date,
-        default: () => new Date()
-      },
-      exercises: [
-        {
-          type: {
-            type: String,
-            trim: true,
-            required: "Enter an exercise type"
-          },
-          name: {
-            type: String,
-            trim: true,
-            required: "Enter an exercise name"
-          },
-          duration: {
-            type: Number,
-            required: "Enter an exercise duration in minutes"
-          },
-          weight: {
-            type: Number
-          },
-          reps: {
-            type: Number
-          },
-          sets: {
-            type: Number
-          },
-          distance: {
-            type: Number
-          }
-        }
-      ]
+// creating  schema
+const WorkoutSchema = new Schema(
+  {
+    day: {
+      type: Date,
+      default: Date.now,
     },
-    {
-      toJSON: {
-        // include any virtual properties when data is requested
-        virtuals: true
-      }
-    }
-  );
-  
-  // adds a dynamically-created property to schema
-  workoutSchema.virtual("totalDuration").get(function () {
-    // "reduce" array of exercises down to just the sum of their durations
-    return this.exercises.reduce((total, exercise) => {
-      return total + exercise.duration;
-    }, 0);
-  });
-  
-  const Workout = mongoose.model("Workout", workoutSchema);
-  
-  module.exports = Workout;
+    // exercises could vary
+    exercises: [
+      {
+        type: {
+          type: String,
+          trim: true,
+          required: "Enter an exercise type",
+        },
+        name: {
+          type: String,
+          trim: true,
+          required: true,
+        },
+        duration: {
+          type: Number,
+          required: true,
+        },
+        weight: {
+          type: Number,
+        },
+        reps: {
+          type: Number,
+        },
+        sets: {
+          type: Number,
+        },
+        distance: {
+          type: Number,
+        },
+      },
+    ],
+  },
+  {
+    toJSON: { virtuals: true },
+  }
+);
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
