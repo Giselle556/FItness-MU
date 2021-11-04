f5a0ccdda2192ac4153444d676c2c35a9587fe6b
Giselle556 <giselleborbolla@yahoo.com>
2021-11-03 21:30:20 -0700

    fixed api route

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,69 +1,61 @@
-// TODO: confirm these are working!
 const router = require("express").Router();
-const db = require("../models");
+const Workout = require("../models/workout.js");
 
-
-
-router.get("/api/workouts", (req, res) => {
-  db.Workout.aggregate([
-    {
-      $addFields: {
-        totalDuration: { $sum: "$exercises.duration" },
-      },
-    },
-  ])
-    .then((workouts) => {
-      res.json(workouts);
-    })
-    .catch((err) => {
-      res.status(400).json(err);
-    });
-});
-
+// get workouts in range
 
 router.get("/api/workouts/range", (req, res) => {
-  db.Workout.aggregate([
+  Workout.aggregate([
     {
       $addFields: {
-        totalDuration: { $sum: "$exercises:duration" },
+        totalDuration: {
+          $sum: "$exercises.duration",
+        },
       },
     },
   ])
     .sort({ _id: -1 })
-    .limit(10)
-    .then((stats) => {
-      res.json(stats);
+    .limit(7)
+    .then((data) => res.json(data))
+    .catch((err) => res.json(err));
+});
+//get all exercises and add up the durations
+
+router.get("/api/workouts/", (req, res) => {
+  Workout.aggregate([
+    {
+      $addFields: {
+        totalDuration: {
+          $sum: "$exercises.duration",
+        },
+      },
+    },
+  ])
+    .then((data) => {
+      res.json(data);
     })
-    .catch((err) => {
-      res.status(400).json(err);
-    });
+    .catch((err) => res.json(err));
 });
 
-// Create a new workout
-router.post("/api/workouts", ({ body }, res) => {
-  console.log(body);
-  db.Workout.create({})
-    .then((newWorkout) => {
-      res.json(newWorkout);
+//create a new work out
+
+router.post("/api/workouts/", (req, res) => {
+  Workout.create({})
+    .then((data) => {
+      res.json(data);
     })
-    .catch((err) => {
-      res.status(400).json(err);
-    });
+    .catch((err) => res.json(err));
 });
 
-// Route to add exercise to current workout
-router.put("/api/workouts/:id", (req, res) => {
-  db.Workout.findByIdAndUpdate(
-    req.params.id,
-    { $push: { exercises: req.body } },
-    { new: true }
+router.put("/api/workouts/:id", ({ body, params }, res) => {
+  Workout.findByIdAndUpdate(
+    params.id,
+    { $push: { exercises: body } },
+    { new: true, runValidators: true }
   )
-    .then((updateWorkout) => {
-      res.json(updateWorkout);
+    .then((data) => {
+      res.json(data);
     })
-    .catch((err) => {
-      res.status(400).json(err);
-    });
+    .catch((err) => res.json(err));
 });
 
 
